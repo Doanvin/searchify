b45c66f404275f07f6608a0b5ace6bbe2d4f08dd
Donavin <donavinhannon@gmail.com>
2018-07-10 22:06:45 -0700

    Started spotify search api integration

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
@@ -10,6 +11,19 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    // choose which api endpoint to fetch (search-type value)
+    const searchType = form.value.searchType;
+    if (searchType == 'artist') {
+      this.fetchSearch(form.value.query, searchType);
+    }
+  }
+  
+  fetchSearch(name, type) {
+    // make api call to https://api.spotify.com/v1/search
+    // ex. ?q=name:query&type=seachType
   }
 
 }
