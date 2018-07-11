3e5be0cf58ec67749da5d2762e7b35ce8b6f18e8
Donavin <donavinhannon@gmail.com>
2018-07-10 21:22:57 -0700

    Basic UI implemented for prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes:Routes = [
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
